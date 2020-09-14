cd07253a1e3ffee4fb00f8be648fa4c830fc9da7
Harry <harry.galliano@zoopla.co.uk>
2020-09-14 14:29:17 +0100

    fixed formatting

--- src/RoverExplorer/RoverExplorer.test.tsx
+++ src/RoverExplorer/RoverExplorer.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
-import { RoverExplorer } from "./RoverExplorer"
+import { RoverExplorer } from "./RoverExplorer";
 
 test('renders Rover explorer link', () => {
     const { getByText } = render(<RoverExplorer />);
