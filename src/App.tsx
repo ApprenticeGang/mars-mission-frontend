0746e031144b04f104567794735dd09137e8e83e
Harry <harry.galliano@zoopla.co.uk>
2020-09-15 11:41:05 +0100

    made it waaaaaayyyyyy nicer

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,55 @@
 import React from "react";
 import styles from './App.module.scss';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
-import {Home} from "./Homepage/Homepage";
-import {RoverExplorer} from "./RoverExplorer/RoverExplorer";
-import {TimelineID} from "./TimelineID/TimelineID";
+import { Home } from "./Homepage/Homepage";
+import { RoverExplorer } from "./RoverExplorer/RoverExplorer";
+import { TimelineID } from "./TimelineID/TimelineID";
 
 
 export default function App() {
-  return (
-    <Router>
-      <div className={styles.navbar}>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/rover_explorer">Rover explorer</Link>
-          </li>
-          <li>
-            <Link to="/timeline/opportunity">Opportunity</Link>
-          </li>
-          <li>
-            <Link to="/timeline/spirit">Spirit</Link>
-          </li>
-          <li>
-            <Link to="/timeline/curiosity">Curiosity</Link>
-          </li>
-          <li>
-            <Link to="/timeline/perserverance">Perserverance</Link>
-          </li>
-        </ul>
+    return (
+        <Router>
+                <nav>
+                    <ul className={styles.navbarUl}>
+                        <li className={styles.navbarLi}>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li className={styles.navbarLi}>
+                            <Link to="/rover_explorer">Rover explorer</Link>
+                        </li>
+                        <li className={styles.navbarLi}>
+                            <Link to="/timeline/opportunity">Opportunity</Link>
+                        </li>
+                        <li className={styles.navbarLi}>
+                            <Link to="/timeline/spirit">Spirit</Link>
+                        </li>
+                        <li className={styles.navbarLi}>
+                            <Link to="/timeline/curiosity">Curiosity</Link>
+                        </li>
+                        <li className={styles.navbarLi}>
+                            <Link to="/timeline/perserverance">Perserverance</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-        <Switch>
-          <Route path="/rover_explorer">
-            <RoverExplorer />
-          </Route>
-          <Route path="/timeline/:id">
-           {<TimelineID />}
-            </Route> 
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+                <Switch>
+                    <Route path="/rover_explorer">
+                        <RoverExplorer />
+                    </Route>
+                    <Route path="/timeline/:id">
+                        {<TimelineID />}
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+        </Router>
+    );
 }
 
 
--- src/TimelineID/TimelineID.tsx
+++ src/TimelineID/TimelineID.tsx
@@ -1,8 +1,4 @@
 import React, { useState } from "react";
-import {
-    BrowserRouter as Router,
-    useParams
-} from "react-router-dom";
 import styles from './TimelineID.module.scss';
 
 interface TimelineID {
@@ -11,7 +7,7 @@ interface TimelineID {
 
 
 export function TimelineID() {
-    const [id, setid] = useState("");
+    const [id] = useState("");
     return (
         <div className={styles.timelineID}>
             <h3>The current Timeline Id is: {id}</h3>
