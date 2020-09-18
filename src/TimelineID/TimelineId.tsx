eaa9b3257ff3269f551ef50b1078967186b590ea
Humphrey Codrington <humphrey.codrington@gmail.com>
2020-09-18 16:07:34 +0100

    pushing timeline branch with timeline to congifure and some state to fix

--- src/TimelineID/TimelineId.tsx
+++ src/TimelineID/TimelineId.tsx
@@ -3,42 +3,51 @@ import styles from './TimelineID.module.scss';
 import {roverCurisity, roverOpportunity, roverPerservance, roverSpirt} from './RoverInformation' 
 import { RoverExplorer } from "../RoverExplorer/RoverExplorer";
 import { useParams } from "react-router-dom";
+import ReactDOM from 'react-dom';
+import Timeline from 'react-image-timeline';
 
-<<<<<<< HEAD
-=======
-
+require('react-image-timeline/dist/timeline.css'); // .scss also available
 
 interface Rover {
     name: string;
     information: string;
     imageUrl: string;
 }
->>>>>>> 11561178191b5d3281d34e6f7d85df652f6538a4
 export const TimelineId: FunctionComponent = () => {
 
-    const {id}: any = useParams();
+    const events = [
+        {
+            date: new Date(2013, 9, 27),
+            text: "Sed leo elit, pellentesque sit amet congue quis, ornare nec lorem.",
+            title: "Cairo, Egypt",
+            buttonText: 'Click Me',
+            imageUrl: "http://github.com/aaron9000/react-image-timeline/blob/master/src/assets/cairo.jpg?raw=true",
+            onClick: console.log,
+        }
+    ];
 
-    const [roverName, setRoverName] = useState<Rover>();
-    useEffect(() => {
-        fetch(`http://localhost:3000/timeline/${id}`)
-        .then(() => setRoverName(roverName?id:String))
+    return <Timeline events={events} />
+    // const {id}: any = useParams();
+    // const [roverName, setRoverName] = useState<Rover>();
+    // useEffect(() => {
+    //     fetch(`http://localhost:3000/timeline/${id}`)
+    //     .then(() => setRoverName(roverName?id:String))
 
-      }, []);
-    return (
-        <div className={styles.timelineID}>
-            <h3>The current Timeline Id is: {id} </h3>
-            <RoverImage name="Spirit" information="Rover on mars" imageUrl= "/images/mars-exploration-rover.png"/>
-        </div>
-    );
+    //   }, []);
+    // return (
+    //     <div className={styles.timelineID}>
+    //         <h3>The current Timeline Id is: {id} </h3>
+    //         <RoverImage name="Spirit" information="Rover on mars" imageUrl= "/images/mars-exploration-rover.png"/>
+    //     </div>
+    // );
 }
 
-export const RoverImage: FunctionComponent<Rover> = (props) => {
-    return (
-        <section>
-            <h2>Rover:</h2>
-            <h2>{props.name}</h2>
-            <h3>{props.information}</h3>
-            <img src = {props.imageUrl}/>
-        </section>
-    )
-};
+// export const RoverImage: FunctionComponent<Rover> = (props) => {
+//     return (
+//         <section>
+//             <h2>{props.name}</h2>
+//             <h3>{props.information}</h3>
+//             <img src = {props.imageUrl}/>
+//         </section>
+//     )
+// };
