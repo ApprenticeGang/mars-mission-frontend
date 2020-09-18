39b73659407eadc2cd8f6b35ba5a255af7a99f3f
Ella Bailo <ella.bailo@zoopla.co.uk>
2020-09-18 09:10:14 +0100

    fixed some warnings

--- src/RoverExplorer/RoverExplorer.test.tsx
+++ src/RoverExplorer/RoverExplorer.test.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import {
     BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
+  
 } from "react-router-dom";
 
 import { RoverExplorer } from "./RoverExplorer";
