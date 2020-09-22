5582d7c9524cb853359533c91d7e66e56136a014
Humphrey Codrington <humphrey.codrington@gmail.com>
2020-09-22 17:25:34 +0100

    working tests, reactored timeline...finally

--- src/TimelineID/TimelineID.test.tsx
+++ src/TimelineID/TimelineID.test.tsx
@@ -4,23 +4,6 @@ import { TimelineId } from './TimelineId';
 import { MemoryRouter, Route } from 'react-router-dom'
 import { mockAllIsIntersecting } from 'react-intersection-observer/test-utils';
 
-
-
-
-// beforeAll(()=> {
-//     const observe = jest.fn();
-//     const unobserve = jest.fn();
-//     const disconnect = jest.fn();
-// // you can also pass the mock implementation
-// // to jest.fn as an argument
-// window.IntersectionObserver = jest.fn(() => ({
-//     disconnect,
-//     observe,
-//     unobserve,
-// })) as any
-
-// })
-
 test('renders Rover Timeline', () => {
     const queries = render(<MemoryRouter initialEntries={["/timeline/perserverance"]}>
         
