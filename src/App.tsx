81085210583a456987b1b0dccf5044357b489991
Ella Bailo <ella.bailo@zoopla.co.uk>
2020-09-22 16:06:01 +0100

    fixed some merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,14 @@ import {
 } from "react-router-dom";
 import { RoverExplorer } from "./RoverExplorer/RoverExplorer";
 import { TimelineId } from "./TimelineID/TimelineId";
-import { ImageViewer } from "./ImageViewer/ImageViewer";
 import HamburgerMenu from "./Components/Hamburger/Hamburger";
 import Navbar from "./Components/Nav/Nav";
-
-=======
-
+import {ImageViewer} from "./ImageViewer/ImageViewer";
 import { Home } from './Homepage/Homepage';
-
-
 
 
 export const App: FunctionComponent = () => {
     const [menuOpen, setMenuOpen] = useState(false);
-
-   
-       return (
-           
-           <Router>
-               <Navbar />
-               <HamburgerMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-               <div onClick={(): void => {setMenuOpen(false)}} data-testid= "Clear nav">
-               <Switch>
-                   <Route path="/rover_explorer">
-                       <RoverExplorer />
-                   </Route>
-                   <Route path="/timeline/:id">
-                       {<TimelineId />}
-                   </Route>
-                   <Route exact path="/">
-                       <Link to="/rover_explorer" className= {styles.marsbutton}> ENTER MARS</Link>
-                       <Mars />
-                   </Route>
-                       <Route exact path="/view_all">
-                          <ImageViewer/> 
-                       </Route>
-               </Switch>
-               </div> 
-           </Router>
-       );
-   }
-   
-
 
     return (
         
@@ -62,7 +28,7 @@ export const App: FunctionComponent = () => {
                 <Route path="/timeline/:id">
                     {<TimelineId />}
                 </Route>
-                <Route path="/">
+                <Route exact path="/">
                     <Home />
                 </Route>
                 <Route exact path="/view_all">
@@ -73,5 +39,4 @@ export const App: FunctionComponent = () => {
         </Router>
     );
 }
-
 
